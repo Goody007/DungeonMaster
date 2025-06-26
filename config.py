ef2626552b9139c8c33a60b0afd8e95dedfd225a
eq <eqSoftware@protonmail.com>
2025-06-26 19:11:14 +0300

    Backported Menu

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import pygame
+from pygame.locals import *
+
 # Файл з константами та конфігурацією
 
 # Розміри вікна
@@ -8,7 +11,7 @@ WINDOW_HEIGHT = 500
 BLACK = (0, 0, 0)
 
 # Швидкості руху
-PLAYER_SPEED = 3
+PLAYER_SPEED = 12
 
 # Ігрові налаштування
 FPS = 15
@@ -20,8 +23,38 @@ LOSE_IMAGE = "background/Loser.png"
 WIN_FON_IMAGE = "background/dungeon_menu.jpeg"
 LOSE_FON_IMAGE = "background/pause_fon.png"
 
-# Розміри об'єктів
+# Menu assets
+MENU_BG = "menu/bg/main_1.jpg"
+MENU_BUTTON_START_PASSIVE = "menu/buttons/start_passive.png"
+MENU_BUTTON_START_ACTIVE = "menu/buttons/start_active.png"
+MENU_BUTTON_EXIT_PASSIVE = "menu/buttons/quit_passive.png"
+MENU_BUTTON_EXIT_ACTIVE = "menu/buttons/quit_active.png"
+MENU_BUTTON_SETTINGS_PASSIVE = "menu/buttons/settings_passive.png"
+MENU_BUTTON_SETTINGS_ACTIVE = "menu/buttons/settings_active.png"
+MENU_TITLE = "menu/DungeonMaster_480.png"
+MENU_MUSIC = "menu/main_menu_sound.mp3"
+
+# Розміри об'єктів (STABLE SIZES - PROPER HITBOXES)
 PLAYER_SIZE = (36, 52)
 ENEMY_SIZE = (27, 42)
 BULLET_RIGHT_SIZE = (31, 5)
 BULLET_LEFT_SIZE = (31, 5)
+
+# Керування
+CONTROLS = {
+    'MOVE_UP': K_w,
+    'MOVE_DOWN': K_s,
+    'MOVE_LEFT': K_a,
+    'MOVE_RIGHT': K_d,
+    'ATTACK_LEFT': K_LEFT,
+    'ATTACK_RIGHT': K_RIGHT
+}
+
+# CONTROLS = {
+#     'MOVE_UP': K_e,
+#     'MOVE_DOWN': K_d,
+#     'MOVE_LEFT': K_s,
+#     'MOVE_RIGHT': K_f,
+#     'ATTACK_LEFT': K_SPACE,
+#     'ATTACK_RIGHT': K_RETURN
+# }
--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-from game import Game
+import pygame
+import sys
+from menu import Menu
+
+def main():
+    try:
+        menu = Menu()
+        menu.run()
+    finally:
+        pygame.quit()
+        sys.exit()
 
 if __name__ == "__main__":
-    game = Game()
-    game.run()
+    main()
